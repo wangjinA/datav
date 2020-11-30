710c14efde41d331a8e36ba091f1c9d9e655baed
小汪同学 <1161520645@qq.com>
2020-11-30 18:08:21 +0800

    .

--- src/myAxios/index.js
+++ src/myAxios/index.js
@@ -2,7 +2,7 @@
  * @Author: 汪锦
  * @Date: 2020-06-19 11:32:06
  * @LastEditors: 汪锦
- * @LastEditTime: 2020-11-30 17:54:02
+ * @LastEditTime: 2020-11-30 18:07:40
  * @Description: 通过原生fetch封装请求
  */
 import { Message } from 'view-design'
@@ -10,15 +10,13 @@ const qs = require('qs')
 const requestAPI = (url, options, showInfo = false) => {
   // const headers = {
   // }
-  if (options.body instanceof FormData) {
-    // options.headers = {
-    //   'content-type': 'multipart/form-data'
-    // }
-  } else {
-    // options.body = JSON.stringify(options.body)
+  if (!(options.body instanceof FormData)) {
+    options.headers = {
+      'content-type': 'application/json',
+    }
+    options.body = JSON.stringify(options.body)
   }
   const _option = {
-    body: options.data, // must match 'Content-Type' header
     cache: 'reload', // *default, no-cache, reload, force-cache, only-if-cached
     //reload 表示本次请求忽略浏览器已经有的缓存（相当于 Ctrl + R 强制刷新），但本次请求的结果还是会遵循响应的 Cache-Control 标头的值来进行缓存存储。
     // 跟 no-store 的不同点在于 no-store 本次请求强制刷新了，下次如果另一个请求 Cache-Control 再指定成别的值比如 only-if-cached，完全不会命中缓存，
@@ -29,16 +27,11 @@ const requestAPI = (url, options, showInfo = false) => {
     // redirect: 'follow', // manual, *follow, error
     // referrer: 'no-referrer', // *client, no-referrer
     credentials: 'include',//为了在当前域名内自动发送 cookie ， 必须提供这个选项
-    // headers: {
-    //   'content-type': 'application/json',
-    // },
     ...options,
   }
   console.log(_option);
   return fetch(url, _option).then(response => {
-    console.log(response);
     return response.json().then(res => {
-      console.log(showInfo, res.message);
       if (showInfo && res.message) {
         Message[res.status ? 'success' : 'error']({
           content: res.message,
