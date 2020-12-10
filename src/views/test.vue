964634f7cda9f10b010c33a9a6f5a375d1aa321d
小汪同学 <1161520645@qq.com>
2020-12-10 16:24:40 +0800

    Merge branch 'main' of https://github.com/wangjinA/datav into main

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
     <Loading />
-    <img width="500" src="@/assets/lizi.png" alt="" />
+    <img width="200" src="@/assets/lizi.png" alt="" />
     <div>测试柜</div>
     <small>
       测试柜
       <small>测试柜</small>
-      <small>测试柜</small>
     </small>
+    <Line-module></Line-module>
   </div>
 </template>
 
 <script>
 import Loading from "@/components/loading/Loading";
+import LineModule from "@/components/lineModule";
 export default {
   components: {
     Loading,
+    LineModule
   },
 };
 </script>
-
-<style></style>
